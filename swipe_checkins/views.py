41ab5c540f833f0e7913d17cd28194c9c5ef3c14
Zachary Voase <z@zacharyvoase.com>
2013-05-14 12:01:16 +0100

    Implemented UserRFIDs outside the admin.

--- swipe_checkins/views.py
+++ swipe_checkins/views.py
@@ -1,12 +1,58 @@
+import re
+
+from django import forms
 from django.conf import settings
 from django.core.exceptions import SuspiciousOperation
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from twilio.util import RequestValidator
 from urlobject import URLObject
 
 from . import models
+
+
+RFID_RE = re.compile(r'^[A-F0-9]+$', re.I)
+
+
+class RFIDForm(forms.Form):
+    rfid = forms.CharField(max_length=40, label="RFID")
+
+    def clean_rfid(self):
+        rfid = self.cleaned_data['rfid']
+        if not RFID_RE.match(rfid):
+            raise forms.ValidationError(
+                "RFID may only contain hex chars (0-9, A-F)")
+        return rfid.upper()
+
+
+def list_rfids(request):
+    if not request.foursquare.is_authenticated:
+        return redirect('/login/')
+    if request.method == 'POST':
+        add_rfid_form = RFIDForm(request.POST)
+        if add_rfid_form.is_valid():
+            request.foursquare.user.rfids.get_or_create(
+                rfid=add_rfid_form.cleaned_data['rfid'].upper())
+            return redirect('/')
+    else:
+        add_rfid_form = RFIDForm()
+    return render(request, 'rfids_list.html', {
+        'user': request.foursquare.user,
+        'add_rfid_form': add_rfid_form})
+
+
+def delete_rfid(request):
+    if not request.foursquare.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        form = RFIDForm(request.POST)
+        if form.is_valid():
+            rfid = form.cleaned_data['rfid']
+            request.foursquare.user.rfids.filter(rfid=rfid).delete()
+    return redirect('/')
 
 
 def validate_twilio_request(request):
--- swipecheck/urls.py
+++ swipecheck/urls.py
@@ -5,7 +5,11 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', 'swipe_checkins.views.list_rfids', name='list_rfids'),
+    url(r'^delete-rfid/$', 'swipe_checkins.views.delete_rfid', name='delete_rfid'),
     url(r'^login/$', 'swipe_foursquare.views.login', name='login'),
     url(r'^oauth-redirect/$', 'swipe_foursquare.views.oauth_redirect', name='oauth-redirect'),
     url(r'^sms/$', 'swipe_checkins.views.sms_received', name='twilio-sms'),

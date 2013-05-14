9eaf7360bafa4e871113ee35bdaaa66309ef2109
Zachary Voase <z@zacharyvoase.com>
2013-05-14 10:33:57 +0100

    Couple of settings tweaks.

--- swipecheck/settings.py
+++ swipecheck/settings.py
@@ -6,7 +6,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Zachary Voase', 'z@zacharyvoase.com'),
 )
 
 MANAGERS = ADMINS
@@ -20,7 +20,7 @@ DATABASES = {
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/London'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html

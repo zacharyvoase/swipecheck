8dabecaa10c92e7974ccd5e40222a4169af25063
Zachary Voase <z@zacharyvoase.com>
2013-05-15 12:03:37 +0200

    Fleshed out the admin a bit more.

--- swipe_checkins/admin.py
+++ swipe_checkins/admin.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Checkin)
-admin.site.register(models.Location)
+class CheckinAdmin(admin.ModelAdmin):
+    list_display = ('from_number', 'rfid', 'created')
+admin.site.register(models.Checkin, CheckinAdmin)
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('device_number', 'venue_id')
+admin.site.register(models.Location, LocationAdmin)
+
+class UserRFID(admin.ModelAdmin):
+    list_display = ('user', 'rfid')
 admin.site.register(models.UserRFID)
--- swipe_foursquare/admin.py
+++ swipe_foursquare/admin.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from . import models
 
 
+class FoursquareUserAdmin(admin.ModelAdmin):
+    list_display = ('user_id',)
 admin.site.register(models.FoursquareUser)
